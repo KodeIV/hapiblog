c113915261628dd56d131e7864ac01c17aa9f0b0
Natalie Newton <natali.newton523@gmail.com>
2014-10-21 15:17:39 +0100

    files reorganised

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 var Hapi = require("hapi");
 var joi = require("joi");
+var routes = require("./routes/routes.js");
+var server = new Hapi.Server(8080, "localhost");
 var viewpoints = {
     engines: {
         jade: require("jade")
     },
 	path: "./views"
 }
-var server = new Hapi.Server(8080, "localhost");
+
+module.exports = server;
 
 server.views(viewpoints)
 
-server.start(function() {
-    console.log("Hapi server started @", server.info.uri);
-
-    server.route({
-	    path: "/hello",
-	    method: "GET",
-	    handler: function(request, reply) {
-	        
-	        //var names = request.params.name.split("/");
-	        reply.view("hello", {
-	            first: "Dan",
-	            last: "Sofer",
-	            mood: "neutral",
-	            colour: "green"
-	        });
-	    }
+if(!module.parent){
+	server.start(function() {
+    	console.log("Hapi server started @", server.info.uri);
 	});
+}
+   
+routes.forEach(function (route){
+	server.route(route);
 });
