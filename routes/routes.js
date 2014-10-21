471a12869aee3cdf0e3b93538369d283297787c0
Naomi Gaynor <naomi.gaynor@gmail.com>
2014-10-21 16:18:18 +0100

    addition of max's routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,45 @@
 var handlers = require("./handlers.js"); 
 
- module.exports = {
+module.exports = {
 
 
- server.route({
-	    path: "/hello",
-	    method: "GET",
-	    handler: function(request, reply) {
-	        
-	        //var names = request.params.name.split("/");
-	        reply.view("hello", {
-	            first: "Dan",
-	            last: "Sofer",
-	            mood: "neutral",
-	            colour: "green"
-	        });
-	    }
+	server.route({
+		path: "/home",
+		method: "GET",
+		handler: function(request, response){
+			handlers.home(request, response);
+		} 
 	});
 
+	server.route({
+		method:'GET',
+		path:'/articles/',
+		handler: function(request, reply){
+			handlers.articles(request, response);
+		}
+	})
+
+	server.route({
+		method:'GET',
+		path:'/articles/{i}/'
+		handler: function(request, reply){
+			reply()
+		}
+	})
+
+	server.route({
+		method:'GET',
+		path:'/articles/new/'
+		handler: function(request, reply){
+			reply()
+		}
+	})
+
+	server.route({
+		method:'GET',
+		path:'/articles/edit/'
+		handler: function(request, reply){
+			reply()
+		}
+	})
 }
