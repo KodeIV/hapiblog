80149a7dfe6de3e5ac883c781a7e3f5ae465200d
Natalie Newton <natali.newton523@gmail.com>
2014-10-30 19:48:36 +0000

    facebook button to login

---
+++ plugins/auth/auth.js
@@ -0,0 +1,26 @@
+var Routes = require("./route.js");
+
+exports.register = function(plugin, options, next) {
+
+    plugin.auth.strategy('facebook', 'bell', {
+                provider: 'facebook',
+                password: 'hapiauth',
+                clientId: '562715117207744', 
+                clientSecret: 'f33a41096a843319154ccb80ece9104e', 
+                isSecure: false 
+            });
+
+    plugin.auth.strategy('session', 'cookie', {
+        password: 'hapiauth', // 
+        cookie: 'sid-hapiauth', // 
+        redirectTo: '/',
+        isSecure: false,
+    });
+
+    plugin.route(Routes);
+    next();
+};
+
+exports.register.attributes = {
+    pkg: require("./package.json")
+};
